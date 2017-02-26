92e4de8051b3619feb18dcf27cd60cab18db1538
Aprilmei <sanchiyou@msn.cn>
2017-02-26 14:45:34 +0000

    Change the setup and add a package

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name="A3Mei_Fangxue_15210672",
+setup(name="A3",
       version="0.1",
       description="Basic system information for COMP30670",
       url="",
       author="Mei Fangxue",
       author_email="sanchiyou@msn.cn",
       licence="GPL3",
+      package=['A3'],
       entry_points={
-          'console_scripts':['A3Mei_Fangxue_15210672=A3Mei_Fangxue_15210672.light_test:switch_light']
+          'console_scripts':['A3Mei_Fangxue_15210672=A3.light_test:switch_light']
           }
       #Github  https://github.com/Aprilmei/Assignment3.git
       )
