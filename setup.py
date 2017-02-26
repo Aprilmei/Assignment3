ad09cf3feb833c4ec1bc5fdbb75951f4367f0fd0
Aprilmei <sanchiyou@msn.cn>
2017-02-26 15:22:06 +0000

    Change the entry_points to led_tester

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name="A3",
       licence="GPL3",
       package=['A3'],
       entry_points={
-          'console_scripts':['A3Mei_Fangxue_15210672=A3.light_test:switch_light']
+          'console_scripts':['led_tester=A3.light_test:switch_light']
           }
       #Github  https://github.com/Aprilmei/Assignment3.git
       )
