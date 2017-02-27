15ac554244227afe80e29634aa8ebe4032397257
Aprilmei <sanchiyou@msn.cn>
2017-02-27 09:13:29 +0000

    CHANGE THE NAME of setup name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name="led_tester",
+setup(name="A3",
       version="0.2",
       description="Led test for science building",
       url="",
       author="Mei Fangxue",
       author_email="sanchiyou@msn.cn",
       licence="GPL3",
-      package=['src'],
+      package=['A3'],
       entry_points={
           'console_scripts':['led_tester=A3.light:switch_light']
           }
