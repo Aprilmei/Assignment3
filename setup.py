d445114e3b6dbdce3f90d086d30f752090c4d6a3
Aprilmei <sanchiyou@msn.cn>
2017-02-27 12:18:21 +0000

    fixing package names

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name="A3",
+setup(name="ledtester",
       version="0.2",
       description="Led test for science building",
       url="",
       author="Mei Fangxue",
       author_email="sanchiyou@msn.cn",
       licence="GPL3",
-      package=['A3'],
+      package=['ledtester'],
       entry_points={
-          'console_scripts':['led_tester=A3.light:switch_light']
+          'console_scripts':['led_tester=ledtester.light:switch_light']
           }
       #Github  https://github.com/Aprilmei/Assignment3.git
       )
